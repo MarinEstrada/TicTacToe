eac1b57b6163f23a3d1a638273e4954ed46dabb6
MarinEstrada <adrian.marin.estrada@gmail.com>
2023-07-16 22:37:44 -0700

    ADD: more variables, also add pngs and implementation to load pngs
    pngs are provided by MrWayFarOut via dropbox for following along with
    his tutorial

--- src/com/marinestrada/tictactoe/TicTacToe.java
+++ src/com/marinestrada/tictactoe/TicTacToe.java
@@ -3,14 +3,28 @@ package com.marinestrada.tictactoe;
 import java.util.Scanner;
 import javax.swing.*;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import java.awt.Font;// to be able to specify fonts
+
+//importing to get images
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 //by immplementing runnable a runnable object is used by default
 public class TicTacToe implements Runnable{
 
     //variables for game
+
+    //note how all variables are private at start,
+    //is better practice to start all vairables as private by default
+    //slowly make variables protected or public as needed
+
     private String ip = "localhost";
     private int port = 2222; //default port
     private Scanner scanner = new Scanner(System.in); // will allow user to use non default port
@@ -47,14 +61,47 @@ public class TicTacToe implements Runnable{
     private int firstSpot = -1; //used to draw winning line accross 3 sequential shapes
     private int secondSpot = -1;//used to draw winning line accross 3 sequential shapes
 
+    
+    private Font font = new Font("Verdana", Font.BOLD, 32);
+    private Font smallerFont = new Font("Verdana", Font.BOLD, 20);
+    private Font largerFont = new Font("Verdana", Font.BOLD, 50);
+
+    private String waitingString = "Waiting for another player";
+    private String unableToCommunicateWithOppStr = "Unable to communicate with opponent.";
+    private String wonString = "You won!!";
+    private String enemyWonString = "Fatality...";
+
     //constructor
     public TicTacToe() {
+        System.out.println("Please input the IP: ");
+        ip = scanner.nextLine();
+        System.out.println("Please input the port: ");
+        port = scanner.nextInt();
+        // if port is out of bounds, it can not exist
+        while(port < 1 || port > 65535){ //note, ports can only be positive shorts
+            System.out.println("Invalid port, please input a valid port: ");
+            port = scanner.nextInt();
+        }
 
+        //next step is to load images
+        loadImages();
     }
 
     //required when implemting runable
     public void run() {
 
+    }
+
+    private void loadImages(){
+        try {
+            board = ImageIO.read(getClass().getResourceAsStream("/board.png"));
+            redX = ImageIO.read(getClass().getResourceAsStream("/redX.png"));
+            redCircle = ImageIO.read(getClass().getResourceAsStream("/redCircle.png"));
+            blueX = ImageIO.read(getClass().getResourceAsStream("/blueX.png"));
+            blueCircle = ImageIO.read(getClass().getResourceAsStream("/blueCircle.png"));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
     
     //main method
